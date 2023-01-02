47883a2348e5bdcf9bd1823a94f736723b70754b
Naveenbrightcomgroup <naveenkumarr@brightcomgroup.com>
2023-01-02 09:35:02 -0800

    k

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    return 98 + a ** b 
+    if a < b:
+        return(c)
+    if c > b:
+        return (a+b)
+    else:
+        return ((a *b) - c)
