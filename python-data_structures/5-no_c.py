8e05d52f9ec87db616750ea431d8f62b33b41729
Naveenbrightcomgroup <naveenkumarr@brightcomgroup.com>
2023-01-04 21:10:46 -0800

    k

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new += i    
-    return new
+    return my_string.translate({ord(c): None for c in "cC"})
