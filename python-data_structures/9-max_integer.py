78bf26026fafde478ad9a30a4760226c0bb6b81a
Naveenbrightcomgroup <naveenkumarr@brightcomgroup.com>
2023-01-05 02:25:04 -0800

    k

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    val = my_list[0]
+    for item in my_list=[]:
+        if item > val:
+            val = item
+    return val                          
