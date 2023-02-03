873c223132a9e41b2f3e62944799fba748c12fd9
Naveenbrightcomgroup <naveenkumarr@brightcomgroup.com>
2023-02-03 00:47:44 -0800

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,8 @@
 CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IF NOT EXISTS user_0d_2@localhost
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+   TO user_0d_2@localhost;
