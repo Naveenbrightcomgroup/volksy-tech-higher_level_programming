939439cc93a76642f40a06b7ce8bcdfbb2424ac2
Naveenbrightcomgroup <naveenkumarr@brightcomgroup.com>
2023-02-08 21:09:21 -0800

    k

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""
-Lists all values in the states tables of a database where name
-matches the argument
-"""
+
+"""displays all values in the states table of hbtn_0e_0_usa
+   where name matches matches an argument passed as a parameter"""
+
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-
-    cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
+    if len(sys.argv) >= 5:
+        db_connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db_connection.cursor()
+        state_name = sys.argv[4]
+        cursor.execute(
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
+        )
+        for data in cursor.fetchall():
+            print(data)
+        cursor.close()
+        db_connection.close()
